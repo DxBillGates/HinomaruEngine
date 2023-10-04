fd2d201e773b6bd6098cecec634d23815f40332f
good_billgates <tomopi364@gmail.com>
2023-10-05 00:32:20 +0900

    create my test project

---
+++ TestProjects/gates/TestProject/CoreTypes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace HE
+{
+	using Bool = bool;
+
+	using Int8 = char;
+	using Int16 = short;
+	using Int32 = int;
+	using Int64 = long long;
+
+	using UInt8 = unsigned char;
+	using UInt16 = unsigned short;
+	using UInt32 = unsigned int;
+	using UInt64 = unsigned long long;
+}
---
+++ TestProjects/gates/TestProject/main.cpp
@@ -0,0 +1,5 @@
+#include "CoreTypes.h"
+
+int main()
+{
+}
